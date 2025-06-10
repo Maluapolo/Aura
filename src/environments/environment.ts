cd0d2aab4c40bdf5ae02674ee453e039ec9127b2
Maluapolo <marialuisaapolinario98@gmail.com>
2025-06-10 18:15:01 -0300

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
+// frontend/src/environments/environment.prod.ts
+
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBwbb5xCyFXY6xJnc_h6ZcMTSH_PpXmvVs",
+    apiKey: "AIzaSyBwbb5xCyFXY6xJnc_h6ZcMTSH_PpXmvVs", // Pode ser a mesma ou uma chave de produ\u00E7\u00E3o diferente
     authDomain: "aura-39c7c.firebaseapp.com",
     projectId: "aura-39c7c",
     storageBucket: "aura-39c7c.firebasestorage.app",
@@ -9,5 +11,5 @@ export const environment = {
     appId: "1:487223881086:web:ee99c3be60d553879e172f",
     measurementId: "G-SDWY8KRSH7"
   },
-  openWeatherApiKey: '786dc9bc4397af18d588973aa6356bda'
+  openWeatherApiKey: '786dc9bc4397af18d588973aa6356bda' // <--- Certifique-se que esta linha est\u00E1 aqui!
 };
